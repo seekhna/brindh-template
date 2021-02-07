0cfe1e92c471de87f575665063e73d6147b93463
Sailaja Nagireddy <snagireddy@agnosy.org>
2021-02-08 02:58:25 +0530

    - Updated the link to which the logo is pointing to.

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -62,7 +62,7 @@ export default function Header(props) {
     [classes.fixed]: fixed
   });
   const brandComponent = (
-    <Link href="/components" as="/components">
+    <Link href="/home" as="/home">
       <Button className={classes.title}>{brand}</Button>
     </Link>
   );
